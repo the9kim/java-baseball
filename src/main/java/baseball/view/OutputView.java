b09082d1a9d8f7e17c2c66dda2b0dc55ff2d7828
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-28 09:54:59 +0900

    feat: 결과 출력 기능 구현 및 테스트

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,45 @@
+package baseball.view;
+
+import baseball.domain.Result;
+
+public class OutputView {
+
+    private static final String STRIKE_COMMENT = "스트라이크";
+    private static final String BALL_COMMENT = "볼";
+    private static final String NOTHING_COMMENT = "낫싱";
+
+    public static void printGameResult(Result result) {
+        int strikeCount = result.getStrike().getStrikeCount();
+        int ballCount = result.getBall().getBallCount();
+        boolean nothingCount = result.getNothing().checkNothing();
+        printStrikeOnly(strikeCount, ballCount);
+        printBallOnly(strikeCount, ballCount);
+        printStrikeAndBall(strikeCount, ballCount);
+        printNothing(nothingCount);
+    }
+
+    private static void printStrikeOnly(int strikeCount, int ballCount) {
+        if (strikeCount > 0 && ballCount == 0) {
+            System.out.printf("%s"+STRIKE_COMMENT+"\n", strikeCount);
+        }
+    }
+
+    private static void printBallOnly(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount > 0) {
+            System.out.printf("%s"+BALL_COMMENT+"\n", ballCount);
+        }
+    }
+
+    private static void printStrikeAndBall(int strikeCount, int ballCount) {
+        if (strikeCount > 0 && ballCount > 0) {
+            System.out.printf("%s"+BALL_COMMENT+" %s"+STRIKE_COMMENT+"\n", ballCount, strikeCount);
+        }
+    }
+
+    private static void printNothing(boolean nothingCount) {
+        if (nothingCount) {
+            System.out.println(NOTHING_COMMENT);
+        }
+    }
+
+}
---
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -0,0 +1,54 @@
+package baseball.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import baseball.domain.Result;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest {
+
+    Result result;
+
+    @BeforeEach
+    void setUp() {
+        result = new Result();
+    }
+
+
+    @DisplayName("스트라이크 카운트만 있을 경우 결과가 정상 출력된다")
+    @Test
+    void printStrikeCountOnly() {
+
+        result.updateResult(3, 0);
+
+        OutputView.printGameResult(result);
+    }
+
+    @DisplayName("볼 카운트만 있을 경우 결과가 정상 출력된다.")
+    @Test
+    void printBallCountOnly() {
+
+        result.updateResult(0, 2);
+
+        OutputView.printGameResult(result);
+    }
+
+    @DisplayName("스트라이크와 볼 카운트 모두 있을 경우 결과가 정상 출력된다.")
+    @Test
+    void printStrikeAndBallCount() {
+
+        result.updateResult(1, 2);
+
+        OutputView.printGameResult(result);
+    }
+
+    @DisplayName("스트라이크와 볼 카운트 모두 없을 경우 결과가 정상 출력된다.")
+    @Test
+    void printNotingCount() {
+
+        result.updateResult(0, 0);
+
+        OutputView.printGameResult(result);
+    }
+}
