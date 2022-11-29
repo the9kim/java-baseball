068ebcaf3d40dc160a4daebbe520f65e2e03edb6
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-29 15:48:53 +0900

    feat: 사용자 입력 야구번호 생성 중

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -6,7 +6,10 @@ import baseball.domain.RandomNumberGenerator;
 public class GameController {
 
     public static void run() {
-        GameNumber gameNumber = RandomNumberGenerator.generateRandomNumbers();
+            GameNumber randomGameNumber = RandomNumberGenerator.generateRandomNumbers();
+            playGame(randomGameNumber);
+    }
 
+    private static void playGame(GameNumber randomGameNumber) {
     }
 }
