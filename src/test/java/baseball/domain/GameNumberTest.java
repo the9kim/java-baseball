c44f78470e047c6576f4d7ce24c5e560b80a1fd6
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-30 10:45:56 +0900

    test: 사용자 입력 야구번호 테스트

--- src/test/java/baseball/domain/GameNumberTest.java
+++ src/test/java/baseball/domain/GameNumberTest.java
@@ -2,10 +2,15 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameNumberTest {
 
@@ -15,12 +20,39 @@ class GameNumberTest {
         List<Integer> randomNumbers = List.of(1,2,3);
 
         GameNumber gameNumber = GameNumber.of(randomNumbers);
-
         for (GameDigit digit : gameNumber.getGameNumber()) {
-            System.out.println(digit.getDigit());
+            System.out.println(digit);
             assertThat(digit.getDigit()).isBetween(1,3);
         }
+    }
 
+    @DisplayName("야구 숫자로 '공백', '문자', '범위 초과 수'를 입력하면 예외가 발생한다.")
+    @ValueSource(strings = {"", "a23", "012", "112"})
+    @ParameterizedTest
+    void constructGameNumberWithWrongInput(String input) {
+        assertThatThrownBy(() -> {
+            GameNumber.of(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("야구 숫자로 '중복된 수' '세자리가 아닌 숫자'를 입력하면 예외가 발생한다.")
+    @ValueSource(strings = {"112", "12", "1234"})
+    @ParameterizedTest
+    void constructGameNumberWithWrongNumber(String input) {
+        assertThatThrownBy(() -> {
+            GameNumber.of(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("야구 숫자로 올바른 수를 입력하면 사용자 번호 객체가 생성된다.")
+    @ValueSource(strings = {"123", "456", "789"})
+    @ParameterizedTest
+    void constructGameNumberWithCorrectInput(String input) {
+        GameNumber a = GameNumber.of(input);
+        for (GameDigit b : a.getGameNumber()) {
+            System.out.println(System.identityHashCode(b));
+        }
+        assertThat(GameNumber.of(input)).isInstanceOf(GameNumber.class);
     }
 
 }
